b1a8040c435c96c3545737f0d489e22af3dc8026
Cecilia <earwen1986@gmail.com>
2022-03-09 10:18:58 +0000

    Initial twitter listener and mock listener

--- twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter/to/kafka/services/TwitterToKafkaServiceApplication.java
+++ twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter/to/kafka/services/TwitterToKafkaServiceApplication.java
@@ -1,6 +1,7 @@
 package com.microservices.demo.twitter.to.kafka.services;
 
 import com.microservices.demo.twitter.to.kafka.services.config.TwitterToKafkaServiceConfigData;
+import com.microservices.demo.twitter.to.kafka.services.runner.StreamRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -16,8 +17,11 @@ public class TwitterToKafkaServiceApplication implements CommandLineRunner {
 
     private final TwitterToKafkaServiceConfigData twitterToKafkaServiceConfigData;
 
-    public TwitterToKafkaServiceApplication(TwitterToKafkaServiceConfigData twitterToKafkaServiceConfigData) {
+    private final StreamRunner streamRunner;
+
+    public TwitterToKafkaServiceApplication(TwitterToKafkaServiceConfigData twitterToKafkaServiceConfigData, StreamRunner streamRunner) {
         this.twitterToKafkaServiceConfigData = twitterToKafkaServiceConfigData;
+        this.streamRunner = streamRunner;
     }
 
     public static void main(String[] args) {
@@ -29,5 +33,6 @@ public class TwitterToKafkaServiceApplication implements CommandLineRunner {
         LOG.info("App starts");
         LOG.info(Arrays.toString(twitterToKafkaServiceConfigData.getTwitterKeywords().toArray(new String[] {})));
         LOG.info(twitterToKafkaServiceConfigData.getWelcomeMessage());
+        streamRunner.start();
     }
 }
